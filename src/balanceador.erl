925b4bb750b570ce997c8b174e5ea96523ed6351
Chauron5 <jloureirocal96@gmail.com>
2017-04-18 11:18:44 +0200

    Cliente para varios balanceadores

--- src/balanceador.erl
+++ src/balanceador.erl
@@ -5,33 +5,23 @@
 
 -define(TIMEOUT, 3000).
 
-% Return node() system load.
-load() -> %1.
-    load_result(cpu_sup:avg1()). %int con la ultima carga(minuto) o  {error, reason} y 0 si no disponible
-
-load_result(0) ->
-    cpu_sup:start(), {load, node(), cpu_sup:avg1()};
-    %node() -> devuelve el nombre del nodo local
-
-load_result(N) ->
-    {load, node(), N}.
-
-
+is_process_alive() ->
+  {node(), self(), erlang:is_process_alive(self())}.
 
 select_server(NodeList, Cliente) ->
-  {Results, BadNodes} = rpc:multicall(NodeList, erlang, is_process_alive, [], ?TIMEOUT),
-  N = random:uniform(length(Results)), %escogemos un nodo aleatorio que va de 1 a N (long de la lista)
+  {Results, _BadNodes} = rpc:multicall(NodeList, balanceador, is_process_alive, [], ?TIMEOUT),
+  N = rand:uniform(length(Results)), %escogemos un nodo aleatorio que va de 1 a N (long de la lista)
   io:format("Numero de nodo aleatorio: ~tp ~n",[N]),
-  Node = lists:nth(N, Results),
-  {servidor, Node} ! {peticion,Cliente}. %sacamos el nodo de la posicion aleatoria N
+  {Node, _, _} = lists:nth(N, Results),
+  {servidor, Node} ! {peticion, Cliente}. %sacamos el nodo de la posicion aleatoria N
 
 start(NodeList) ->
     register (balanceador, spawn (?MODULE, loop , [NodeList])), ok.
 
 loop(NodeList) ->
   receive
-    {peticion, Cliente} -> 
-      select_server(NodeList, Cliente), 
+    {peticion, Cliente} ->
+      select_server(NodeList, Cliente),
       loop(NodeList);
     _ -> fail
   end.
--- src/cliente.erl
+++ src/cliente.erl
@@ -2,11 +2,16 @@
 
 %-export([start/0, start/1, calculate/1]).
 -compile(export_all).
+-define(TIMEOUT, 3000).
 
+calcular(Nodos) ->
+  {Results, _BadNodes} = rpc:multicall(Nodos, balanceador, is_process_alive, [], ?TIMEOUT),
+  {Nodo, _, _} = lists:nth(1, Results),
+  calcular_aux(Nodo).
 
-calcular(Nodo) ->
+calcular_aux(Nodo) ->
   io:format("Enviada peticion...~n"),
- 
+
   {balanceador, Nodo} ! {peticion, self()},
   io:format("Esperando respuesta...~n"),
   receive
