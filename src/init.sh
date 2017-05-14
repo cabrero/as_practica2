c764dfeb34ff70ff7327f662cfab1ee48f5fe3a0
fabian.lema <fabian.lema@udc.es>
2017-05-14 22:32:52 +0200

    Corregido

--- src/init.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-erlc balanceador.erl cliente.erl servidor.erl
- xterm erl -s servidor start -sname s
- xterm erl -s servidor start -sname s2
- xterm erl -s servidor start -sname s3
- xterm erl -s balanceador start -sname b [s@MBP, s2@MBP, s3@MBP]
- xterm erl -s cliente calcular -sname c [b@MBP]
